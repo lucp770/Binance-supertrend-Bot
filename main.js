37194bfe598aca191f917b1e56b268d2c8322dc4
Lucas P. F <35705412+lucp770@users.noreply.github.com>
2022-12-12 21:24:51 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+const continueButton = document.querySelector('.continue-button');
+
+continueButton.addEventListener('click', ()=>{
+	alert('botao clicado');
+
+	// try{
+
+	// const apiKey = 'JpGtN1hXUZ67JJNAfcRfQ0iAJw4vEoYJZgRW5hg19hUf9wRoyaGn2l4Gy2pRJCJU';
+	// const apiSecret = 'A6J2tIKlUYr9XXUl59lnLvF7AXVSE5F1bIgl1saApCxsAqsbeQ6HDyR89ErqB6ei';
+	// const exchange = new ccxt.binance({'apiKey': apiKey, 'secret': apiSecret});
+	// // console.log(exchange);
+	// let data = exchange.fetch_ohlcv('ETH/BTC', limit = "10");
+	// console.log(data);
+	// }catch{
+	// 	alert('erro de atutenticação!!!');
+	// }
+})
