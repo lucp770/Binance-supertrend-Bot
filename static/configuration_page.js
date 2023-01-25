d32db13e40b811b5575ae83f6bbc4319d4a1debd
Lucas <lucp770@gmail.com>
2023-01-25 19:03:04 -0300

    implemented the validation for the user assets and ammount

--- static/configuration_page.js
+++ static/configuration_page.js
@@ -6,6 +6,9 @@ const startBtn = document.querySelector('.start-tradding-btn');
 const userInputAmount = document.querySelector('.trading-amount-class');
 const assets = document.querySelectorAll('.asset');
 
+const comboBox = document.querySelector('.combo');
+
+
 
 // array of objects for user available assets.
 const userAvailableAssets = [];
@@ -23,11 +26,61 @@ function constructAssetsObj(){
 constructAssetsObj();
 
 function checkAmount(){
+	// get the inserted ammoun
+
+	let selectedCoin= comboBox.value
+	
+
+	// get the selling and buying asset
+	if (selectedCoin != 'Select'){
+
+	let separator = selectedCoin.search('/');
+	let sellAsset = selectedCoin.substring(separator+1, selectedCoin.length);
+	let buyAsset = selectedCoin.substring(0,separator);
+
+	console.log(userInputAmount)
+
+	let traddingAmmount = Number(userInputAmount.value);
+
+	let match = userAvailableAssets.find(coin => coin.asset === sellAsset);
+
+	if(match === undefined){
+		alert('please check if you have the selling asset in your account');
+	}
+	else{
+		// check if the user has enough
+		if(Number(match.total) < traddingAmmount){
+			alert('Please check if you have in your account the amount selected to trade')
+			// make the border of the asset become red
+		}
+		else{
+			console.log('user can trade');
+		}
+
+	}
+	console.log(match)
+
+
+
+
+	// check if user has the assets
+	console.log('assets', userAvailableAssets)
+
+
+	}
+
+	// compare both numerically
+
+}
+
+function showPlot(){
 	console.log('clicado');
 }
 
 
+
+
 startBtn.addEventListener('click', ()=>{
-	// checkAmount();
+	checkAmount();
 	
 })
